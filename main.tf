d1a331d1085173c996ca5054fa13aa525597b125
Karl Schriek <karl.schriek@bma.network>
2024-10-03 13:02:13 +0200

    3

--- main.tf
+++ main.tf
@@ -3,3 +3,7 @@ resource random_pet  m3 {}
 module "m2" {
     source = "git@github.com:karlschriek/module-tests-2.git"
 }
+
+module "m22" {
+    source = "git@github.com:karlschriek/module-tests-2.git/module22"
+}
